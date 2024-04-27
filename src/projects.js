548208755e7649da05e71e615ecd5f8a0bea861f
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2024-04-27 02:55:07 -0500

    Move projects to projects page

---
+++ src/projects.js
@@ -0,0 +1,27 @@
+fetch('../data/projects.json')
+    .then(response => response.json())
+    .then(projects => {
+        const container = document.getElementById('projects-container');
+        
+        projects.forEach(project => {
+            let contents = `<section class="project">
+            <img src="${project.image}" alt="${project.title} Image">
+            <div>
+                <h2><a href="project.html?id=${project.id}">${project.title}</a></h2>
+                <p>${project.description}</p>
+                <p><b>Date:</b> ${project.date}</p>
+                <p><b>Motivation:</b> ${project.motivation}</p>
+                <p><b>Technologies:</b> ${project.tech}</p>`;
+            // omit these if empty
+            if(project.link.length > 0) {
+                contents += `<p><b>Link:</b> <a href="${project.link}">${project.link}</a></p>`;
+            }
+            if(project.repo.length > 0) {
+                contents += `<p><b>Repo:</b> <a href="${project.repo}">${project.repo}</a></p>`;
+            }
+            contents += `</div>
+            </section>`;
+            
+            container.innerHTML += contents;
+        });
+    });
