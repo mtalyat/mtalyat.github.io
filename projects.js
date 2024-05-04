52b964ec9b122128d85ec2c1ea9d07eda8c531d9
Mitchell Talyat <49891937+mtalyat@users.noreply.github.com>
2024-05-04 14:41:40 -0500

    Change projects to grid layout

--- projects.js
+++ projects.js
@@ -4,23 +4,33 @@ fetch('../data/projects.json')
         const container = document.getElementById('projects-container');
         
         projects.forEach(project => {
-            let contents = `<section class="project">
-            <img src="${project.image}" alt="${project.title} Image">
-            <div>
-                <h2><a href="project.html?id=${project.id}">${project.title}</a></h2>
-                <p>${project.description}</p>
-                <p><b>Date:</b> ${project.date}</p>
-                <p><b>Motivation:</b> ${project.motivation}</p>
-                <p><b>Technologies:</b> ${project.tech}</p>`;
-            // omit these if empty
-            if(project.link.length > 0) {
-                contents += `<p><b>Link:</b> <a href="${project.link}">${project.link}</a></p>`;
-            }
-            if(project.repo.length > 0) {
-                contents += `<p><b>Repository:</b> <a href="${project.repo}">${project.repo}</a></p>`;
-            }
-            contents += `</div>
-            </section>`;
+            // let contents = `<section class="project">
+            // <img src="${project.image}" alt="${project.title} Image">
+            // <div>
+            //     <h2><a href="project.html?id=${project.id}">${project.title}</a></h2>
+            //     <p>${project.description}</p>
+            //     <p><b>Date:</b> ${project.date}</p>
+            //     <p><b>Motivation:</b> ${project.motivation}</p>
+            //     <p><b>Technologies:</b> ${project.tech}</p>`;
+            // // omit these if empty
+            // if(project.link.length > 0) {
+            //     contents += `<p><b>Link:</b> <a href="${project.link}">${project.link}</a></p>`;
+            // }
+            // if(project.repo.length > 0) {
+            //     contents += `<p><b>Repository:</b> <a href="${project.repo}">${project.repo}</a></p>`;
+            // }
+            // contents += `</div>
+            // </section>`;
+
+            let contents = `<div class="project">
+            <img src="${project.image}" alt="${project.title} image">
+            <a href="project.html?id=${project.id}">
+            <div class="overlay">
+            <h1>${project.title}</h1>
+            <p>${project.description}</p>
+            </div>
+            </a>
+            </div>`
             
             container.innerHTML += contents;
         });
